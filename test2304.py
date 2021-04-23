38354d478e982f923480e56a7d7ac462ae70e07b
harriet <harriet.edery@efrei.net>
2021-04-23 12:10:13 +0200

    xieme init

---
+++ test2304.py
@@ -0,0 +1,13 @@
+import unittest
+from Calculator import Calculator
+
+class Tests(unittest.TestCase):
+
+    def test_calculator(self):
+
+        self.assertEqual(Calculator.add(1,0),1)
+        self.assertEqual(Calculator.add(1,1),2)
+        self.assertEqual(Calculator.add(1.5,1.5),3)
+
+if __name__=='__main__':
+    unittest.main()
